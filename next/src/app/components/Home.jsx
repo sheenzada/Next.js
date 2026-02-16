4700043f4f7bd76a14333cc7805ae4879aa875fd
Inamullah <sheenzada7@gmail.com>
2026-02-16 03:47:24 -0800

    Next js

--- next/src/app/components/Home.jsx
+++ next/src/app/components/Home.jsx
@@ -1,30 +1,42 @@
+import Image from "next/image";
+import profilePic from "./inam-2.jpg";
 
 export default function HomePage() {
   return (
-    <div id="about" className="min-h-screen bg-blue-400 from-blue-500 to-purple-600 flex items-center justify-center px-6">
-      <div>
-        <img src="src/app/components/inam-2.jpg" alt="Image" className="w-60 h-60 rounded-full bg-amber-500 mx-auto items-center"/>
+    <div
+      id="about"
+      className="min-h-screen bg-blue-400 from-blue-500 to-purple-600 flex flex-col md:flex-row items-center justify-center px-6 gap-12"
+    >
+      {/* Image Section */}
+      <div className="w-60 h-60 flex">
+        <Image
+          src={profilePic}
+          alt="Profile Image"
+          className="rounded-full object-center"
+        />
       </div>
-      <div className="text-center text-white max-w-2xl">
+
+      {/* Text Section */}
+      <div className="text-center md:text-left text-white max-w-2xl">
         <h1 className="text-5xl font-bold mb-6">
-          Welcome to My Website 
+          Welcome to My Website
         </h1>
 
-        <p className="text-white text-lg mb-8">
+        <p className="text-lg mb-8">
           We build modern, fast and beautiful websites using Next.js and React.
         </p>
 
-        <div className="flex justify-center gap-6">
+        <div className="flex justify-center md:justify-start gap-6">
           <a
             href="/about"
-            className="bg-white text-blue-600 px-6 py-3 rounded-lg font-semibold hover:bg-black transition"
+            className="bg-white text-blue-600 px-6 py-3 rounded-lg font-semibold hover:bg-black hover:text-white transition"
           >
             About Us
           </a>
 
           <a
             href="/service"
-            className="bg-black text-blue-500 px-6 py-3 rounded-lg font-semibold hover:bg-white transition"
+            className="bg-black text-blue-500 px-6 py-3 rounded-lg font-semibold hover:bg-white hover:text-black transition"
           >
             Our Services
           </a>
